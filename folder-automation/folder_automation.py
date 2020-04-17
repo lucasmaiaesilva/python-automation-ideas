b26c5286a046e8551e120d457e037cc342ba112b
Lucas Silva <lucassilva@MacBook-Air-de-Lucas.local>
2020-04-17 11:21:02 -0300

    add watchdog feature working

--- folder-automation/folder_automation.py
+++ folder-automation/folder_automation.py
@@ -11,25 +11,29 @@ new_destination = "/Users/lucassilva/Developer/python/folder_automation/content"
 
 os.makedirs("/Users/lucassilva/Developer/python/folder_automation/content", exist_ok=True)
 
-class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
+
+if __name__ == "__main__":
+    def update_folder():
         for filename in os.listdir(folder_to_track):
             filename_parts = filename.split('.')
             extension = filename_parts[-1]
             is_directory = len(filename_parts) == 1
-            # print(is_directory)
             src = folder_to_track + "/" + filename
             if (not is_directory and extension != 'app'):
                 shutil.copyfile(src, new_destination + "/" + filename)
 
-event_handler = MyHandler()
-Observer().schedule(event_handler, folder_to_track, recursive=True)
-Observer().start()
+    def on_modified_folder(event):
+        update_folder()
+        print("the folder has been modified")
 
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    Observer().stop()
-
-Observer().join()
+    event_handler = FileSystemEventHandler()
+    event_handler.on_modified = on_modified_folder
+    observer = Observer()
+    observer.schedule(event_handler, folder_to_track, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
