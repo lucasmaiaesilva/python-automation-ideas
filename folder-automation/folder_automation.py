7badc8082c93c607ecb3b33c0111d4a69b8b7166
Lucas Silva <lucasmaiaesilva@gmail.com>
2020-04-18 14:11:53 -0300

    feat: separate files by extension

--- folder-automation/folder_automation.py
+++ folder-automation/folder_automation.py
@@ -7,25 +7,28 @@ import shutil
 import time
 
 folder_to_track = "/Users/lucassilva/Downloads"
-new_destination = "/Users/lucassilva/Developer/python/folder_automation/content"
+new_destination = "/Users/tmp/content"
 
-os.makedirs("/Users/lucassilva/Developer/python/folder_automation/content", exist_ok=True)
+os.makedirs(new_destination, exist_ok=True)
+
+def update_folder():
+    for filename in os.listdir(folder_to_track):
+        filename_parts = filename.split('.')
+        extension = filename_parts[-1]
+        is_directory = len(filename_parts) == 1
+        src = folder_to_track + "/" + filename
+        if (not is_directory and extension != 'app'):
+            path_with_extension = new_destination + "/" + extension
+            os.makedirs(path_with_extension, exist_ok=True)
+            print("Creating dir: {}".format(extension))
+            shutil.copyfile(src, path_with_extension + "/" + filename)
+
+def on_modified_folder(event):
+    update_folder()
+    print("the folder has been modified")
 
 
 if __name__ == "__main__":
-    def update_folder():
-        for filename in os.listdir(folder_to_track):
-            filename_parts = filename.split('.')
-            extension = filename_parts[-1]
-            is_directory = len(filename_parts) == 1
-            src = folder_to_track + "/" + filename
-            if (not is_directory and extension != 'app'):
-                shutil.copyfile(src, new_destination + "/" + filename)
-
-    def on_modified_folder(event):
-        update_folder()
-        print("the folder has been modified")
-
     event_handler = FileSystemEventHandler()
     event_handler.on_modified = on_modified_folder
     observer = Observer()
